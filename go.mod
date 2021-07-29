4bcb6ab6bbdd79750106aee944fa9e74c8e258f3
Jack HUANG <jiajue.huang@live.com>
2021-07-29 22:33:57 +0800

    update to use v1.0.0

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/JackKCWong/go-cranker-app
 go 1.15
 
 require (
-	github.com/JackKCWong/go-cranker-connector v0.2.1
-	github.com/klauspost/compress v1.11.13 // indirect
-	github.com/rs/zerolog v1.21.0
+	github.com/JackKCWong/go-cranker-connector v1.0.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/rs/zerolog v1.23.0
 	github.com/tv42/httpunix v0.0.0-20191220191345-2ba4b9c3382c
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
--- main.go
+++ main.go
@@ -12,8 +12,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/JackKCWong/go-cranker-connector/pkg/config"
-	"github.com/JackKCWong/go-cranker-connector/pkg/connector/v1"
+	"github.com/JackKCWong/go-cranker-connector/connector"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/tv42/httpunix"
@@ -23,34 +22,35 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 
-	u := &httpunix.Transport{
+	unixsockettr := &httpunix.Transport{
 		DialTimeout:           100 * time.Millisecond,
 		RequestTimeout:        1 * time.Second,
 		ResponseHeaderTimeout: 1 * time.Second,
 	}
 
 	tlsSkipVerify := &tls.Config{InsecureSkipVerify: true}
-	conn := connector.NewConnector(
-		&config.RouterConfig{
-			TLSClientConfig:   tlsSkipVerify,
-			WSHandshakTimeout: 1 * time.Second,
-		},
-		&config.ServiceConfig{
-			HTTPClient: &http.Client{
-				Transport: u,
-			},
-		})
+
+	conn := connector.Connector{
+		ServiceName:         "myservice",
+		ServiceURL:          "http+unix://myservice",
+		WSSHttpClient:       &http.Client{Transport: &http.Transport{TLSClientConfig: tlsSkipVerify}},
+		ServiceHttpClient:   &http.Client{Transport: unixsockettr},
+		ShutdownTimeout:     5 * time.Second,
+		RediscoveryInterval: 5 * time.Second,
+	}
 
 	crankerWss := os.Args[1]
 
-	err := conn.Connect([]string{crankerWss}, 2, "myservice", "http+unix://myservice")
+	err := conn.Connect(func() []string {
+		return []string{crankerWss}
+	}, 2)
 	if err != nil {
 		fmt.Printf("Error connecting cranker %s, err: %q", crankerWss, err)
 		return
 	}
 
-	os.Remove("unixsocket")
-	usocket, err := net.Listen("unix", "unixsocket")
+	_ = os.Remove("unixsocket")
+	unixsocket, err := net.Listen("unix", "unixsocket")
 	if err != nil {
 		log.Error().AnErr("err", err).Msg("cannot listen on unixsocket")
 	}
@@ -67,24 +67,27 @@ func main() {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		log.Info().Msg("shutting down...")
-		conn.Shutdown(ctx)
+		conn.Shutdown()
 
 		ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
-		srv.Shutdown(ctx)
-
-		srv.Close()
-		usocket.Close()
+		_ = srv.Shutdown(ctx)
+		_ = srv.Close()
+		_ = unixsocket.Close()
 		log.Info().Msg("shutdown finished")
 	}()
 
-	u.RegisterLocation("myservice", "unixsocket")
+	unixsockettr.RegisterLocation("myservice", "unixsocket")
 
-	http.HandleFunc("/myservice/hello", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "hello\n")
+	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(rw, "world\n")
 	})
 
-	if err := srv.Serve(usocket); err != nil {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(rw, "got: %s", r.URL.Path)
+	})
+
+	if err := srv.Serve(unixsocket); err != nil {
 		log.Error().AnErr("err", err).Msg("cannot serve on unixsocket")
 	}
 
